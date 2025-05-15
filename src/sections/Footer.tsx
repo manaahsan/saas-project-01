ec784b8d38772dc792a35c2452df7b25d34d7225
manazir <ahsanmanaz@gmail.com>
2025-05-15 16:09:09 +0400

    key value added

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
           <SocialYoutube />
         </div>
         <p className="mt-6">
-          &copy; 2024 Your Company, Inc. All rights reserved.
+          &copy; 2025 Manazir, Inc. All rights reserved.
         </p>
       </div>
     </footer>
